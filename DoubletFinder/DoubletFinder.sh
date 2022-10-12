fc31e3eeb1d77c92d2137c4a4ac16f3f551ce857
Zhen Zuo <13971930+zhenzuo2@users.noreply.github.com>
2022-10-12 10:15:56 -0500

    pre-processing

---
+++ DoubletFinder/DoubletFinder.sh
@@ -0,0 +1,36 @@
+input_path="/storage/singlecell/zz4/fetal_bash/results/after_qc_seurat_object/"
+sample_id=(
+Multi_Fetal_11w2d_FR
+Multi_Fetal_11w2d_FR_2
+Multi_Fetal_11w2d_NR
+Multi_Fetal_13W_FR
+Multi_Fetal_13W_NR
+Multi_Fetal_14w5d_FR
+Multi_Fetal_14w5d_NR
+Multi_Fetal_19W4d_FR
+Multi_Fetal_19W4d_NR
+Multi_Fetal_20W2d_FR
+Multi_Fetal_20W2d_NR
+Multi_Fetal_23w1d_FR
+Multi_Fetal_23w1d_NR
+Multiome_10w_FR
+Multiome_10w_NR
+Multiome_12w3d_FR
+Multiome_12w3d_NR
+Multiome_14w2d_FR
+Multiome_14w2d_NR
+Multiome_16w4d_FR
+Multiome_16w4d_NR
+Multiome_20w1d_FR
+Multiome_20w1d_NR
+Multiome_23w4d_FR
+Multiome_23w4d_NR
+)
+output_results_path="/storage/singlecell/zz4/fetal_bash/results/DoubletFinder_seurat_object/"
+output_figuress_path="/storage/singlecell/zz4/fetal_bash/figures/DoubletFinder_UMAP/"
+
+
+for f in ${sample_id[@]}
+do
+    slurmtaco.sh -p short -m 5G -t 1 -- /storage/chen/home/zz4/anaconda3/envs/r/bin/Rscript DoubletFinder.R "${input_path}${f}_nFeature_RNA_1000_3000_MT_5_fitered.rds" "$output_results_path" "$output_figuress_path" "$f";
+done
---
+++ DoubletFinder/out_slurm/tmp.wgqSl1UpBb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=tmp.wgqSl1UpBb
+#SBATCH --partition=short
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=5G
+#SBATCH --time=12:00:00
+#SBATCH -o out_slurm/tmp.wgqSl1UpBb-%j.out
+#SBATCH -e out_slurm/tmp.wgqSl1UpBb-%j.err
+
+start=$(date +%s)
+echo "starting at $(date) on $(hostname)"
+
+# Print the SLURM job ID.
+echo "SLURM_JOBID=$SLURM_JOBID"
+
+# Run the application
+/storage/chen/home/zz4/anaconda3/envs/r/bin/Rscript DoubletFinder.R /storage/singlecell/zz4/fetal_bash/results/after_qc_seurat_object/Multi_Fetal_11w2d_NR_nFeature_RNA_1000_3000_MT_5_fitered.rds /storage/singlecell/zz4/fetal_bash/results/DoubletFinder_seurat_object/ /storage/singlecell/zz4/fetal_bash/figures/DoubletFinder_UMAP/ Multi_Fetal_11w2d_NR
+
+end=$(date +%s)
+echo "ended at $(date) on $(hostname). Time elapsed: $(date -u -d @$((end-start)) +'%H:%M:%S')"
+exit 0
